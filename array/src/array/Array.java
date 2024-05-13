aae7e40452693831a176dd255f913dd4399c57be
RAKHI N KUMAR <rakhi.nkumar@gmail.com>
2024-05-13 16:43:23 +0530

    arr1

---
+++ array/src/array/Array.java
@@ -0,0 +1,18 @@
+package array;
+
+public class Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a[]=new int[5];
+		 a[0]=10;
+		 a[1]=20;
+	     a[2]=70;
+		 a[3]=50;
+		 a[4]=30;
+		for(int i=0;i<a.length;i++)
+			System.out.println(a[i]);
+
+	}
+
+}
---
+++ array/src/array/array2.java
@@ -0,0 +1,22 @@
+package array;
+
+public class array2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ int a[]=new int[9];
+   a[0]=10;
+   a[1]=20;
+   a[2]=30;
+   a[3]=40;
+   a[4]=50;
+   a[5]=60;
+   a[6]=70;
+   a[7]=80;
+   a[8]=90;
+   for(int i=0;i<a.length;i++)
+	   System.out.println(a[i]);
+   
+	}
+
+}
